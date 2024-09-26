e04c570fc3134b8263da48efbab6923edfc2f67e
AnikatsuAnime <anikatsu.anime@gmail.com>
2024-09-26 08:52:43 +0530

    Update gogoanime_servers.js

--- src/api/gogoanime_servers.js
+++ src/api/gogoanime_servers.js
@@ -1,6 +1,7 @@
 export const servers = [
-  { id: "streamwish", name: "Streamwish" },
+
   { id: "gogocdn", name: "Gogo server" },
   { id: "vidstreaming", name: "Vidstreaming" },
   { id: "mp4upload", name: "Mp4Upload" },
+    { id: "streamwish", name: "Streamwish" },
 ];
